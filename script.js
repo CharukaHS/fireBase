2b5abf8bb2fc2d0153aa798da21d4582ecaa2fba
CharukaHS <charuka.h.sangeeth@gmail.com>
2024-01-17 14:19:59 +0530

    add CDN script to JS file

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyByxQapsD9ndpy4I4RmMOS9EZR7pmI0A80",
+    authDomain: "webdev---demo.firebaseapp.com",
+    projectId: "webdev---demo",
+    storageBucket: "webdev---demo.appspot.com",
+    messagingSenderId: "589291520580",
+    appId: "1:589291520580:web:15961a61a10eec96def211",
+    measurementId: "G-R0MZX61K4L"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
