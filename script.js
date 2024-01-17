6bf7b6723ebb761bd98bd4ac9ea74da4f2e6f05f
CharukaHS <charuka.h.sangeeth@gmail.com>
2024-01-17 15:23:38 +0530

    get data from firebase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
+  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyByxQapsD9ndpy4I4RmMOS9EZR7pmI0A80",
@@ -14,3 +14,38 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const docList = [];
+  const ul = document.querySelector('ul');
+//   console.log(db);
+
+  const collRef = collection(db, 'programming languages');
+//   console.log(collRef);
+
+  const docSnap = getDocs(collRef);
+  
+  docSnap.then((snapshot) => {
+    snapshot.docs.forEach((docs) => {
+        docList.push([{...docs.data()}])
+    })
+    console.log(docList);
+    addData(docList)
+  })
+
+  const addData = (docList) => {
+
+    docList.forEach((docs) => {
+
+        const query = `<div>
+        
+        <li>${docs[0].name}</li>
+        <li>${docs[0].designed_by}</li>
+        <li>${docs[0].invented_year}</li>
+
+
+        </div>`
+        ul.innerHTML += query
+
+    })
+  }
+  
